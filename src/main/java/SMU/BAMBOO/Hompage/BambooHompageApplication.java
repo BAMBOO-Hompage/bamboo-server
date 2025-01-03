52b5c96d6ffbf8b277241c9e804b0a910eb6bf6e
shash0423 <shash042319@naver.com>
2025-01-03 10:25:53 +0900

    프로젝트 생성

---
+++ src/main/java/SMU/BAMBOO/Hompage/BambooHompageApplication.java
@@ -0,0 +1,13 @@
+package SMU.BAMBOO.Hompage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooHompageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooHompageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SMU/BAMBOO/Hompage/BambooHompageApplicationTests.java
@@ -0,0 +1,13 @@
+package SMU.BAMBOO.Hompage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooHompageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

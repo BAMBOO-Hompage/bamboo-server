a9fab8a334fa06004b9b90df303bfddb98b8de65
shash0423 <shash042319@naver.com>
2025-01-09 21:44:56 +0900

    "feat: Swagger 초기 설정 (#2)"

---
+++ src/main/java/SMU/BAMBOO/Hompage/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package SMU.BAMBOO.Hompage.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("BAMBOO Hompage Backend API Docs")
+                .description("휴먼과 동아리 BAMBOO 홈페이지 API 명세서입니다.");
+    }
+}

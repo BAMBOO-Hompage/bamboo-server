54bb6e404a9ed6f34e7d657b5e625e884b917b1a
1026hz <1026hzz@gmail.com>
2025-01-18 14:19:00 +0900

    fix: Base Entity 추적 위한 EnableJpaAuditing 추가

--- src/main/java/SMU/BAMBOO/Hompage/BambooHompageApplication.java
+++ src/main/java/SMU/BAMBOO/Hompage/BambooHompageApplication.java
@@ -2,8 +2,10 @@ package SMU.BAMBOO.Hompage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BambooHompageApplication {
 
 	public static void main(String[] args) {

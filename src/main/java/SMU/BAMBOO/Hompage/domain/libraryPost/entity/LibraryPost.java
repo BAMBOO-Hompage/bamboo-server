78372b64fc05cc1a719801e3d8da28d54e5f542a
shash0423 <shash042319@naver.com>
2025-01-20 17:22:58 +0900

    feat: 알렉산드리아 도메인에 생성, 수정 추가 #25

--- src/main/java/SMU/BAMBOO/Hompage/domain/libraryPost/entity/LibraryPost.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/libraryPost/entity/LibraryPost.java
@@ -1,5 +1,6 @@
 package SMU.BAMBOO.Hompage.domain.libraryPost.entity;
 
+import SMU.BAMBOO.Hompage.domain.libraryPost.dto.LibraryPostRequestDTO;
 import SMU.BAMBOO.Hompage.domain.mapping.LibraryPostTag;
 import SMU.BAMBOO.Hompage.domain.member.entity.Member;
 import SMU.BAMBOO.Hompage.global.common.BaseEntity;
@@ -44,4 +45,26 @@ public class LibraryPost extends BaseEntity {
     @Builder.Default
     @OneToMany(mappedBy = "libraryPost", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LibraryPostTag> libraryPostTags = new ArrayList<>();
+
+    public static LibraryPost from(LibraryPostRequestDTO.Create request, Member member) {
+        return LibraryPost.builder()
+                .member(member)
+                .speaker(request.speaker())
+                .paperName(request.paperName())
+                .year(request.year())
+                .topic(request.topic())
+                .link(request.link())
+                // .libraryPostTags(request.libraryPostTags())
+                .build();
+    }
+
+    public void update(LibraryPostRequestDTO.Update request) {
+        this.speaker = request.speaker();
+        this.paperName = request.paperName();
+        this.year = request.year();
+        this.topic = request.topic();
+        this.link = request.link();
+        // this.libraryPostTags = request.libraryPostTags();
+    }
+
 }

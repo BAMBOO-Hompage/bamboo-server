1945809d97ea1f3592baf7eb786dab3fcc5e27b1
shash0423 <shash042319@naver.com>
2025-01-20 18:06:27 +0900

    feat: Tag DTO 추가 #27

---
+++ src/main/java/SMU/BAMBOO/Hompage/domain/tag/entity/dto/TagRequestDTO.java
@@ -0,0 +1,13 @@
+package SMU.BAMBOO.Hompage.domain.tag.entity.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "태그 관련 요청 DTO")
+public class TagRequestDTO {
+
+    @Schema(description = "태그 생성 요청 DTO")
+    public record Create(
+            @Schema(description = "태그 이름", example = "CV") String name
+    ) {}
+
+}

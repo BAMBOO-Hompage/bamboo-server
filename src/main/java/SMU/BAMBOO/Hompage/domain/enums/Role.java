0931ae4eff6451509cd9d0e9d4084096a8bbefb0
ee h <eeh@eeui-MacBookPro.local>
2025-01-21 23:05:58 +0900

    chore: ROLE_GUEST 추가

--- src/main/java/SMU/BAMBOO/Hompage/domain/enums/Role.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/enums/Role.java
@@ -1,6 +1,6 @@
 package SMU.BAMBOO.Hompage.domain.enums;
 
 public enum Role {
-    ROLE_USER, ROLE_ADMIN, ROLE_OPS
+    ROLE_GUEST, ROLE_USER, ROLE_ADMIN, ROLE_OPS
 }
-/* 아기판다, 운영진, 임원진(operation) */
+/* 게스트, 아기판다, 운영진, 임원진(operation) */

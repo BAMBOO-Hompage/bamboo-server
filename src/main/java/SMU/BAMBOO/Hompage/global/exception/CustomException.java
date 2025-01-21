3b2c4a247eee49a979eb11b1145cc372743b8b5e
shash0423 <shash042319@naver.com>
2025-01-21 15:48:35 +0900

    fix: CustomException 생성자 수정 #30

--- src/main/java/SMU/BAMBOO/Hompage/global/exception/CustomException.java
+++ src/main/java/SMU/BAMBOO/Hompage/global/exception/CustomException.java
@@ -9,6 +9,7 @@ public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage()); // 넣지 않으면 상위 객체인 Throwable 클래스에서 message가 null로 초기화 - CustomExcepton은 getter가 있어서 괜찮지만 ErrorCode에 직접 접근할 때 null 반환
         this.errorCode = errorCode;
     }
 

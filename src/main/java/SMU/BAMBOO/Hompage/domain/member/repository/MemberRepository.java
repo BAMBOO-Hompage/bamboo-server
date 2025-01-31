6c56283ecfeba57ba717e3cc0f07d019762114b5
shash0423 <shash042319@naver.com>
2025-02-01 00:39:44 +0900

    feat: 회원 삭제, 탈퇴 구현 #61

--- src/main/java/SMU/BAMBOO/Hompage/domain/member/repository/MemberRepository.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import SMU.BAMBOO.Hompage.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +25,6 @@ public interface MemberRepository {
     Page<Member> findAll(Pageable pageable);
 
     Page<Member> findAllSortByRole(Pageable pageable);
+
+    void hardDeleteOldMembers(LocalDateTime threshold);
 }

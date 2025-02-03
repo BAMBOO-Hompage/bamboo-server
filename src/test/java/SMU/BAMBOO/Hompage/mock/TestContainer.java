4f73b8bb7700d1720f608cc351df65c56dfcd4fd
shash0423 <shash042319@naver.com>
2025-02-03 13:40:40 +0900

    test: Tag Controller 테스트 코드 작성 #63

--- src/test/java/SMU/BAMBOO/Hompage/mock/TestContainer.java
+++ src/test/java/SMU/BAMBOO/Hompage/mock/TestContainer.java
@@ -3,6 +3,7 @@ package SMU.BAMBOO.Hompage.mock;
 import SMU.BAMBOO.Hompage.domain.libraryPost.controller.LibraryPostController;
 import SMU.BAMBOO.Hompage.domain.libraryPost.repository.LibraryPostRepository;
 import SMU.BAMBOO.Hompage.domain.libraryPost.service.LibraryPostServiceImpl;
+import SMU.BAMBOO.Hompage.domain.tag.controller.TagController;
 import SMU.BAMBOO.Hompage.domain.tag.repository.TagRepository;
 import SMU.BAMBOO.Hompage.domain.tag.service.TagServiceImpl;
 import lombok.Builder;
@@ -11,9 +12,12 @@ public class TestContainer {
 
     public final LibraryPostRepository libraryPostRepository;
     public final TagRepository tagRepository;
+
     public final LibraryPostServiceImpl libraryPostService;
     public final TagServiceImpl tagService;
+
     public final LibraryPostController libraryPostController;
+    public final TagController tagController;
 
     @Builder
     public TestContainer() {
@@ -24,13 +28,15 @@ public class TestContainer {
                 .libraryPostRepository(this.libraryPostRepository)
                 .tagRepository(this.tagRepository)
                 .build();
-
         this.tagService = TagServiceImpl.builder()
                 .tagRepository(this.tagRepository)
                 .build();
 
         this.libraryPostController = LibraryPostController.builder()
                 .libraryPostService(this.libraryPostService)
+                .build();
+        this.tagController = TagController.builder()
+                .tagService(this.tagService)
                 .build();
     }
 }

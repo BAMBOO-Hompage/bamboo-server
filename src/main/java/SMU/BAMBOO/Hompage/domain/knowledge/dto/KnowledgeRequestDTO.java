329226f7dad844c56673ec83e46584f6e3054f1c
shash0423 <shash042319@naver.com>
2025-02-04 12:29:27 +0900

    feat: 지식공유 게시판 DTO 생성 #71

---
+++ src/main/java/SMU/BAMBOO/Hompage/domain/knowledge/dto/KnowledgeRequestDTO.java
@@ -0,0 +1,28 @@
+package SMU.BAMBOO.Hompage.domain.knowledge.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Schema(description = "지식공유 게시판 요청 DTO")
+public class KnowledgeRequestDTO {
+
+    @Schema(description = "지식공유 게시판 생성 요청 DTO")
+    public record Create(
+            @Schema(description = "제목", example = "Python 지식 공유") String title,
+            @Schema(description = "내용", example = "~Python 지식 방출 중~") String content,
+            @Schema(description = "타입", example = "RESOURCES") String type,
+            @Schema(description = "이미지 파일 목록", example = "[]") List<MultipartFile> images,
+            @Schema(description = "파일 목록", example = "[]") List<MultipartFile> files
+    ) {}
+
+    @Schema(description = "지식공유 게시판 수정 요청 DTO")
+    public record Update(
+            @Schema(description = "제목", example = "수정된 Python 지식 공유") String title,
+            @Schema(description = "내용", example = "~수정된 Python 지식 방출 중~") String content,
+            @Schema(description = "타입", example = "RESOURCES") String type,
+            @Schema(description = "이미지 파일 목록", example = "[]") List<MultipartFile> images,
+            @Schema(description = "파일 목록", example = "[]") List<MultipartFile> files
+    ) {}
+}
---
+++ src/main/java/SMU/BAMBOO/Hompage/domain/knowledge/dto/KnowledgeResponseDTO.java
@@ -0,0 +1,86 @@
+package SMU.BAMBOO.Hompage.domain.knowledge.dto;
+
+import SMU.BAMBOO.Hompage.domain.enums.KnowledgeType;
+import SMU.BAMBOO.Hompage.domain.knowledge.entity.Knowledge;
+import SMU.BAMBOO.Hompage.domain.knowledgeComment.dto.KnowledgeCommentResponse;
+import SMU.BAMBOO.Hompage.domain.member.dto.response.MemberResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Schema(description = "지식공유 게시판 응답 DTO")
+public class KnowledgeResponseDTO {
+    @Schema(description = "지식공유 게시판 생성 응답 DTO")
+    public record Create(
+            @Schema(description = "지식공유 게시판 ID") Long knowledgeId,
+            @Schema(description = "작성한 멤버 정보") MemberResponse member,
+            @Schema(description = "제목") String title,
+            @Schema(description = "내용") String content,
+            @Schema(description = "게시글 유형(RESOURCES, INSIGHTS, CAREERS)") KnowledgeType type,
+            @Schema(description = "이미지 리스트") List<String> images,
+            @Schema(description = "파일 리스트") List<String> files
+    ) {
+        public static KnowledgeResponseDTO.Create from(Knowledge knowledge) {
+            return new KnowledgeResponseDTO.Create(
+                    knowledge.getKnowledgeId(),
+                    MemberResponse.from(knowledge.getMember()),
+                    knowledge.getTitle(),
+                    knowledge.getContent(),
+                    knowledge.getType(),
+                    knowledge.getImage(),
+                    knowledge.getFile()
+            );
+        }
+    }
+
+    @Schema(description = "지식공유 게시판 수정 응답 DTO")
+    public record Update (
+            @Schema(description = "지식공유 게시판 ID") Long knowledgeId,
+            @Schema(description = "[수정] 제목") String title,
+            @Schema(description = "[수정] 내용") String content,
+            @Schema(description = "게시글 유형(RESOURCES, INSIGHTS, CAREERS)") KnowledgeType type,
+            @Schema(description = "이미지 리스트") List<String> images,
+            @Schema(description = "파일 리스트") List<String> files
+    ) {
+        public static KnowledgeResponseDTO.Update from(Knowledge knowledge) {
+            return new KnowledgeResponseDTO.Update(
+                    knowledge.getKnowledgeId(),
+                    knowledge.getTitle(),
+                    knowledge.getContent(),
+                    knowledge.getType(),
+                    knowledge.getImage(),
+                    knowledge.getFile()
+            );
+        }
+    }
+
+    @Schema(description = "지식공유 게시판 단건 조회 응답 DTO")
+    public record GetOne(
+            @Schema(description = "지식공유 게시판 ID") Long knowledgeId,
+            @Schema(description = "작성한 멤버 정보") MemberResponse member,
+            @Schema(description = "제목") String title,
+            @Schema(description = "내용") String content,
+            @Schema(description = "게시글 유형(RESOURCES, INSIGHTS, CAREERS)") KnowledgeType type,
+            @Schema(description = "조회수") int views,
+            @Schema(description = "이미지 리스트") List<String> images,
+            @Schema(description = "파일 리스트") List<String> files,
+            @Schema(description = "댓글 리스트") List<KnowledgeCommentResponse.GetOne> comments
+    ) {
+        public static KnowledgeResponseDTO.GetOne from(Knowledge knowledge) {
+            return new KnowledgeResponseDTO.GetOne(
+                    knowledge.getKnowledgeId(),
+                    MemberResponse.from(knowledge.getMember()),
+                    knowledge.getTitle(),
+                    knowledge.getContent(),
+                    knowledge.getType(),
+                    knowledge.getViews(),
+                    knowledge.getImage(),
+                    knowledge.getFile(),
+                    knowledge.getKnowledgeComments().stream()
+                            .map(KnowledgeCommentResponse.GetOne::from)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+}

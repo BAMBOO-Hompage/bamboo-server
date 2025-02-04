da433a3c51f5f0b8069ce37c65c3813ce537f3ce
shash0423 <shash042319@naver.com>
2025-02-04 18:32:24 +0900

    feat: 엔티티 함수 생성, 수정 #71

--- src/main/java/SMU/BAMBOO/Hompage/domain/knowledge/entity/Knowledge.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/knowledge/entity/Knowledge.java
@@ -1,6 +1,7 @@
 package SMU.BAMBOO.Hompage.domain.knowledge.entity;
 
 import SMU.BAMBOO.Hompage.domain.enums.KnowledgeType;
+import SMU.BAMBOO.Hompage.domain.knowledge.dto.KnowledgeRequestDTO;
 import SMU.BAMBOO.Hompage.domain.knowledgeComment.entity.KnowledgeComment;
 import SMU.BAMBOO.Hompage.domain.member.entity.Member;
 import SMU.BAMBOO.Hompage.global.common.BaseEntity;
@@ -27,7 +28,7 @@ public class Knowledge extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = true)
     private Member member;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String title;
 
     @Column(nullable = false, columnDefinition = "TEXT")
@@ -37,20 +38,50 @@ public class Knowledge extends BaseEntity {
     @Column(length = 15)
     private KnowledgeType type;
 
+    @Builder.Default
     @Column(nullable = false)
-    private int views;
+    private int views = 0;
 
+    @Builder.Default
     @ElementCollection
-    @CollectionTable(name = "main_activities_images", joinColumns = @JoinColumn(name = "main_activities_id"))
+    @CollectionTable(name = "knowledge_images", joinColumns = @JoinColumn(name = "knowledge_id"))
     @Column(name = "image_url")
-    private List<String> image;
+    private List<String> images = new ArrayList<>();
 
+    @Builder.Default
     @ElementCollection
-    @CollectionTable(name = "main_activities_files", joinColumns = @JoinColumn(name = "main_activities_id"))
+    @CollectionTable(name = "knowledge_files", joinColumns = @JoinColumn(name = "knowledge_id"))
     @Column(name = "file_url")
-    private List<String> file;
+    private List<String> files = new ArrayList<>();
 
     @Builder.Default
     @OneToMany(mappedBy = "knowledge", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<KnowledgeComment> knowledgeComments = new ArrayList<>();
+
+    public static Knowledge from(KnowledgeRequestDTO.Create request, Member member, List<String> images, List<String> files) {
+        return Knowledge.builder()
+                .member(member)
+                .title(request.title())
+                .content(request.content())
+                .type(KnowledgeType.valueOf(request.type()))
+                .views(0)
+                .images(images != null ? new ArrayList<>(images) : new ArrayList<>())
+                .files(files != null ? new ArrayList<>(files) : new ArrayList<>())
+                .build();
+    }
+
+    public void update(KnowledgeRequestDTO.Update request,  List<String> newImages, List<String> newFiles) {
+        this.title = request.title();
+        this.content = request.content();
+        this.type = KnowledgeType.valueOf(request.type());
+        if (newImages != null) {
+            this.images.clear();
+            this.images.addAll(newImages);
+        }
+        if (newFiles != null) {
+            this.files.clear();
+            this.files.addAll(newFiles);
+        }
+    }
 }
+

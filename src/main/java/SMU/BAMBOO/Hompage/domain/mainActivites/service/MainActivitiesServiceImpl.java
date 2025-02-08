d473ea432d1ca9dbb07f6f53e73b3a41700514d2
shash0423 <shash042319@naver.com>
2025-02-08 19:54:28 +0900

    test: MainActivities Service 테스트 코드 #84

--- src/main/java/SMU/BAMBOO/Hompage/domain/mainActivites/service/MainActivitiesServiceImpl.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/mainActivites/service/MainActivitiesServiceImpl.java
@@ -10,15 +10,14 @@ import SMU.BAMBOO.Hompage.global.exception.ErrorCode;
 import SMU.BAMBOO.Hompage.global.upload.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Hibernate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.hibernate.Hibernate;
 import org.springframework.web.multipart.MultipartFile;
-
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/SMU/BAMBOO/Hompage/mock/TestContainer.java
+++ src/test/java/SMU/BAMBOO/Hompage/mock/TestContainer.java
@@ -3,26 +3,37 @@ package SMU.BAMBOO.Hompage.mock;
 import SMU.BAMBOO.Hompage.domain.libraryPost.controller.LibraryPostController;
 import SMU.BAMBOO.Hompage.domain.libraryPost.repository.LibraryPostRepository;
 import SMU.BAMBOO.Hompage.domain.libraryPost.service.LibraryPostServiceImpl;
+import SMU.BAMBOO.Hompage.domain.mainActivites.repository.MainActivitiesRepository;
+import SMU.BAMBOO.Hompage.domain.mainActivites.service.MainActivitiesServiceImpl;
 import SMU.BAMBOO.Hompage.domain.tag.controller.TagController;
 import SMU.BAMBOO.Hompage.domain.tag.repository.TagRepository;
 import SMU.BAMBOO.Hompage.domain.tag.service.TagServiceImpl;
+import SMU.BAMBOO.Hompage.global.upload.service.AwsS3Service;
 import lombok.Builder;
+import org.mockito.Mockito;
 
 public class TestContainer {
 
     public final LibraryPostRepository libraryPostRepository;
     public final TagRepository tagRepository;
+    public final MainActivitiesRepository mainActivitiesRepository;
 
     public final LibraryPostServiceImpl libraryPostService;
     public final TagServiceImpl tagService;
+    public final MainActivitiesServiceImpl mainActivitiesService;
 
     public final LibraryPostController libraryPostController;
     public final TagController tagController;
+
+    public final AwsS3Service awsS3Service;
 
     @Builder
     public TestContainer() {
         this.libraryPostRepository = new FakeLibraryPostRepository();
         this.tagRepository = new FakeTagRepository();
+        this.mainActivitiesRepository = new FakeMainActivitiesRepository();
+
+        this.awsS3Service = Mockito.mock(AwsS3Service.class);
 
         this.libraryPostService = LibraryPostServiceImpl.builder()
                 .libraryPostRepository(this.libraryPostRepository)
@@ -31,6 +42,7 @@ public class TestContainer {
         this.tagService = TagServiceImpl.builder()
                 .tagRepository(this.tagRepository)
                 .build();
+        this.mainActivitiesService = new MainActivitiesServiceImpl(this.mainActivitiesRepository, this.awsS3Service);
 
         this.libraryPostController = LibraryPostController.builder()
                 .libraryPostService(this.libraryPostService)

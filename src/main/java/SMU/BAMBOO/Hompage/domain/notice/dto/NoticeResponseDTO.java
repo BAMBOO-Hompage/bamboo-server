91836c6ea77c819ade278f2921cab45b60c3b979
shash0423 <shash042319@naver.com>
2025-02-10 17:07:04 +0900

    feat: 공지사항, 지식공유 응답에 생성, 수정일 추가 #88

--- src/main/java/SMU/BAMBOO/Hompage/domain/notice/dto/NoticeResponseDTO.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/notice/dto/NoticeResponseDTO.java
@@ -5,6 +5,7 @@ import SMU.BAMBOO.Hompage.domain.member.dto.response.MemberResponse;
 import SMU.BAMBOO.Hompage.domain.notice.entity.Notice;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +20,9 @@ public class NoticeResponseDTO {
             @Schema(description = "내용") String content,
             @Schema(description = "게시글 유형(EVENTS, NOTICE)") NoticeType type,
             @Schema(description = "이미지 리스트") List<String> images,
-            @Schema(description = "파일 리스트") List<String> files
+            @Schema(description = "파일 리스트") List<String> files,
+            @Schema(description = "생성일") LocalDateTime createdAt,
+            @Schema(description = "수정일") LocalDateTime updatedAt
     ) {
         public static NoticeResponseDTO.Detail from(Notice notice) {
             return new NoticeResponseDTO.Detail(
@@ -29,7 +32,9 @@ public class NoticeResponseDTO {
                     notice.getContent(),
                     notice.getType(),
                     notice.getImages() != null ? notice.getImages() : new ArrayList<>(),
-                    notice.getFiles() != null ? notice.getFiles() : new ArrayList<>()
+                    notice.getFiles() != null ? notice.getFiles() : new ArrayList<>(),
+                    notice.getCreatedAt(),
+                    notice.getModifiedAt()
             );
         }
     }

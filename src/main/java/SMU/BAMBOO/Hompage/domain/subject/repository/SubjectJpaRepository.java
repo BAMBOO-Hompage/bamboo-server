cd7c1cc22ae15c1fc2a6b49f888aada681f12ff2
shash0423 <shash042319@naver.com>
2025-02-13 19:33:14 +0900

    feat: 커리큘럼 유무로 조회 기능 #106

--- src/main/java/SMU/BAMBOO/Hompage/domain/subject/repository/SubjectJpaRepository.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/subject/repository/SubjectJpaRepository.java
@@ -3,9 +3,11 @@ package SMU.BAMBOO.Hompage.domain.subject.repository;
 import SMU.BAMBOO.Hompage.domain.subject.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubjectJpaRepository extends JpaRepository<Subject, Long> {
 
     Optional<Subject> findByName(String name);
+    List<Subject> findByIsBook(Boolean isBook);
 }

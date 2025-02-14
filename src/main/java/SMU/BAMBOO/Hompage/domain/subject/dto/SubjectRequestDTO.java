d39f7196ad407d18d210a354ad86d2700b534cb5
shash0423 <shash042319@naver.com>
2025-02-14 19:14:06 +0900

    fix: 과목 DTO에 기수 추가 #111

--- src/main/java/SMU/BAMBOO/Hompage/domain/subject/dto/SubjectRequestDTO.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/subject/dto/SubjectRequestDTO.java
@@ -8,12 +8,14 @@ public class SubjectRequestDTO {
     @Schema(description = "과목 생성 요청 DTO")
     public record Create(
             @Schema(description = "과목 이름", example = "PY") String name,
-            @Schema(description = "커리큘럼 유무", example = "true") Boolean isBook
+            @Schema(description = "커리큘럼 유무", example = "true") Boolean isBook,
+            @Schema(description = "기수", example = "true") int batch
     ) {}
 
     @Schema(description = "과목 수정 요청 DTO")
     public record Update(
             @Schema(description = "과목 이름", example = "PY") String name,
-            @Schema(description = "커리큘럼 유무", example = "true") Boolean isBook
+            @Schema(description = "커리큘럼 유무", example = "true") Boolean isBook,
+            @Schema(description = "기수", example = "true") int batch
     ) {}
 }

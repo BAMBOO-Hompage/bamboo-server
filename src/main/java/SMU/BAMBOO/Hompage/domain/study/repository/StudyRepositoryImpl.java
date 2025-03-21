566e557bcf15aae09ec9da8771a570c2674fafcd
shash0423 <shash042319@naver.com>
2025-03-21 21:50:51 +0900

    refactor: Q객체 static import #139

--- src/main/java/SMU/BAMBOO/Hompage/domain/study/repository/StudyRepositoryImpl.java
+++ src/main/java/SMU/BAMBOO/Hompage/domain/study/repository/StudyRepositoryImpl.java
@@ -1,17 +1,18 @@
 package SMU.BAMBOO.Hompage.domain.study.repository;
 
-import SMU.BAMBOO.Hompage.domain.cohort.entity.QCohort;
-import SMU.BAMBOO.Hompage.domain.mapping.memberStudy.entity.QMemberStudy;
-import SMU.BAMBOO.Hompage.domain.member.entity.QMember;
-import SMU.BAMBOO.Hompage.domain.study.entity.QStudy;
 import SMU.BAMBOO.Hompage.domain.study.entity.Study;
-import SMU.BAMBOO.Hompage.domain.subject.entity.QSubject;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import static SMU.BAMBOO.Hompage.domain.cohort.entity.QCohort.cohort;
+import static SMU.BAMBOO.Hompage.domain.mapping.memberStudy.entity.QMemberStudy.memberStudy;
+import static SMU.BAMBOO.Hompage.domain.member.entity.QMember.member;
+import static SMU.BAMBOO.Hompage.domain.study.entity.QStudy.study;
+import static SMU.BAMBOO.Hompage.domain.subject.entity.QSubject.subject;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,12 +23,6 @@ public class StudyRepositoryImpl implements StudyRepository {
 
     @Override
     public Optional<Study> findById(Long id) {
-        QStudy study = QStudy.study;
-        QMemberStudy memberStudy = QMemberStudy.memberStudy;
-        QMember member = QMember.member;
-        QSubject subject = QSubject.subject;
-        QCohort cohort = QCohort.cohort;
-
         Study result = queryFactory
                 .selectFrom(study)
                 .leftJoin(study.subject, subject).fetchJoin()
@@ -42,12 +37,6 @@ public class StudyRepositoryImpl implements StudyRepository {
 
     @Override
     public List<Study> findAll() {
-        QStudy study = QStudy.study;
-        QMemberStudy memberStudy = QMemberStudy.memberStudy;
-        QMember member = QMember.member;
-        QSubject subject = QSubject.subject;
-        QCohort cohort = QCohort.cohort;
-
         return queryFactory
                 .selectFrom(study)
                 .leftJoin(study.subject, subject).fetchJoin()
@@ -61,16 +50,10 @@ public class StudyRepositoryImpl implements StudyRepository {
 
     @Override
     public List<Study> findByCohortAndSubject(Long cohortId, Long subjectId) {
-        QStudy study = QStudy.study;
-        QMemberStudy memberStudy = QMemberStudy.memberStudy;
-        QMember member = QMember.member;
-        QSubject subjectEntity = QSubject.subject;
-        QCohort cohortEntity = QCohort.cohort;
-
         return queryFactory
                 .selectFrom(study)
-                .leftJoin(study.subject, subjectEntity).fetchJoin()
-                .leftJoin(study.cohort, cohortEntity).fetchJoin()
+                .leftJoin(study.subject, subject).fetchJoin()
+                .leftJoin(study.cohort, cohort).fetchJoin()
                 .leftJoin(study.memberStudies, memberStudy).fetchJoin()
                 .leftJoin(memberStudy.member, member).fetchJoin()
                 .where(
